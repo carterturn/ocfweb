194011f8b5fe53dd5a448d924bcfa0a15055e38f
Carter Turnbaugh <carterturn@ocf.berkeley.edu>
2018-03-12 17:30:31 -0700

    Switched storage to yaml instead of SQL

--- ocfweb/api/inventory.py
+++ ocfweb/api/inventory.py
@@ -1,24 +1,15 @@
-from functools import partial
 from ipaddress import ip_address
 from json import loads as json_load_string
 
-from django.conf import settings
 from django.http import HttpResponse
 from django.http import HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from ocflib.infra.hosts import hosts_by_filter
 from ocflib.infra.net import is_ocf_ip
-from ocflib.lab.inventory import get_connection
+from ocflib.lab.inventory import add_host_inventory
 
 from ocfweb.caching import cache
-
-get_connection = partial(
-    get_connection,
-    user=settings.OCFINVENTORY_USER,
-    password=settings.OCFINVENTORY_PASSWORD,
-    db=settings.OCFINVENTORY_DB,
-)
 
 
 @require_POST
@@ -41,33 +32,12 @@ def update_inventory(request):
         if hostname == '' or hostname is None:
             raise ValueError('Invalid host "{}"'.format(hostname))
 
-        for device in hardware['devices']:
-            device_class = device['class']
-            if device_class == '' or device_class is None:
-                raise ValueError('Invalid device class "{}"'.format(device_class))
+        add_host_inventory(hardware)
 
-            device_merchant = device['merchant']
-            if device_merchant == '' or device_merchant is None:
-                raise ValueError('Invalid device merchant "{}"'.format(device_merchant))
-
-            device_name = device['name']
-            if device_name == '' or device_name is None:
-                raise ValueError('Invalid device name "{}"'.format(device_name))
-
-            _add_desktop_device(hostname, device_class, device_merchant, device_name)
-
-        return HttpResponse(status=204)
+        return HttpResponse('')
 
     except ValueError as e:
         return HttpResponseBadRequest(e)
-
-
-def _add_desktop_device(hostname, device_class, device_merchant, device_name):
-    values = '("{hostname}", "{device_class}", "{device_merchant}", "{device_name}", NOW())'.format(
-        hostname=hostname, device_class=device_class, device_merchant=device_merchant, device_name=device_name,
-    )
-    with get_connection() as c:
-        c.execute('INSERT INTO `lab_inventory` VALUES' + values + 'ON DUPLICATE KEY UPDATE `last_seen`=NOW()')
 
 
 @cache()
--- ocfweb/stats/inventory.py
+++ ocfweb/stats/inventory.py
@@ -1,16 +1,5 @@
-from functools import partial
-
-from django.conf import settings
 from django.shortcuts import render
-from ocflib.lab.inventory import get_connection
 from ocflib.lab.inventory import get_devices
-
-get_connection = partial(
-    get_connection,
-    user=settings.OCFINVENTORY_USER,
-    password=settings.OCFINVENTORY_PASSWORD,
-    db=settings.OCFINVENTORY_DB,
-)
 
 
 def hardware(request):
